d44615f072a54ff712793a0768a5e8e72f7ece81
Parth Bhardwaj <parth@Parths-MacBook-Air.local>
2021-02-10 14:08:10 +0530

    index page of zoklean and assets

---
+++ index.js
@@ -0,0 +1,34 @@
+const menuBtn = document.querySelector(".menu-burger");
+let menuOpen = false;
+menuBtn.addEventListener("click", function () {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
+/*===== MENU SHOW Y HIDDEN =====*/
+const navMenu = document.getElementById("nav-menu"),
+  toggleMenu = document.getElementById("nav-toggle"),
+  closeMenu = document.getElementById("nav-close");
+
+/*SHOW*/
+toggleMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
+
+/*HIDDEN*/
+closeMenu.addEventListener("click", () => {
+  navMenu.classList.remove("show");
+});
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  /*Remove menu mobile*/
+  navMenu.classList.remove("show");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
